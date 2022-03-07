71a73b18a4514fa1c6e1ad320a0cbc58f5651d53
TejoBurgboy <tejovdburg@gmail.com>
2022-03-07 16:52:10 +0100

    Blok-Tech

---
+++ static/register.js
@@ -0,0 +1,17 @@
+console.log("aap");
+let naamveld = document.querySelector("#Naamveld")
+let mailveld = document.querySelector("#Mailveld")
+let passwordveld = document.querySelector("#Passwordveld")
+let buttonInput = document.querySelector("input#submit")
+
+let inputform = () =>{
+    let nameinput = document.querySelector("input#name").value;
+    let passwordinput = document.querySelector("input#wachtwoord").value;
+    let mailinput = document.querySelector("input#mail").value;
+    naamveld.textContent = nameinput;
+    mailveld.textContent = mailinput;
+    passwordveld.textContent = passwordinput;
+    console.log(nameinput,passwordinput,mailinput);
+};
+
+buttonInput.addEventListener("click", inputform);
