690e15bad9141766e0043ec59e72f12626e9e8ae
TejoBurgboy <tejovdburg@gmail.com>
2022-03-08 14:46:29 +0100

    Blok-Tech

--- Server.js
+++ Server.js
@@ -4,12 +4,14 @@ const bodyParser = require('body-parser');
 const Port = process.env.Port || 1234;
 const bcrypt = require('bcrypt');
 const { response } = require('express');
+const users = [];
 
 const app = express();
 
 app.use(express.static(__dirname + "/static"));
 app.use(express.urlencoded({ extended:false}))
 
+//Hier word handlebars als engine neergezet
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
@@ -20,14 +22,18 @@ const user = {
   woont: 'ouderkerk'
 };
 
+// het maken van een index pagina
 app.get('/', (req, res) => {
   res.render('index', { user: user });
 });
 
+// het maken van een registeer pagina
 app.get('/register', (req, res) => {
   res.render('register');
 })
 
+// het ophalen van de info van de registeer pagina.
+// Dit is een async functie omdat er try en catch in zitten.
 app.post('/register', async (req, res) => {
 try{
 const hashedPassword = await bcrypt.hash(req.body.wachtwoord, 10);
@@ -35,11 +41,11 @@ users.push({
   id: Date.now().toString(),
   name: req.body.name,
   mail: req.body.mail,
-  password: hashedPassword;
+  password: hashedPassword
 })
 res.redirect('/login');
 } catch{
-res.redirect('/register');
+res.redirect('/about');
 }
 console.log(users);
 });
@@ -57,7 +63,7 @@ app.post('/login', (req, res) => {
 });
 
 
-
+//connecten met de local host
 app.listen(Port, () => {
   console.log(`server running on port: ${Port}`)
 });
--- static/register.js
+++ static/register.js
@@ -1,8 +1,8 @@
 console.log("aap");
-let naamveld = document.querySelector("#Naamveld")
-let mailveld = document.querySelector("#Mailveld")
-let passwordveld = document.querySelector("#Passwordveld")
-let buttonInput = document.querySelector("input#submit")
+let naamveld = document.querySelector("#Naamveld");
+let mailveld = document.querySelector("#Mailveld");
+let passwordveld = document.querySelector("#Passwordveld");
+let buttonInput = document.querySelector("button");
 
 let inputform = () =>{
     let nameinput = document.querySelector("input#name").value;
