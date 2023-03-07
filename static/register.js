31da93757536a380720a1902dcaad2987cdcb97a
TejoBurgboy <tejovdburg@gmail.com>
2023-03-07 14:51:47 +0100

    hoe werkt fetch

--- static/register.js
+++ static/register.js
@@ -2,23 +2,25 @@ const gamekiezer = document.querySelector("#gamechoose");
 const buttonInput = document.querySelector("button");
 const games = ["Mario", "Pokemon", "Gta", "Fifa", "World of war craft", "Zelda", "Kirby", "Super Smash", "Heardstone", "League of Legends", "mario", "pokemon", "LOL", "WOWC", "minecraft", "Civ", "FM", "Football manager"];
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8b3b04ef67mshb9494ce2457e3c4p130cadjsn6c5d39bb1752',
+		'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com'
+	}
+};
+
+fetch('https://rawg-video-games-database.p.rapidapi.com/games', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+
 let inputform = () => {
 	console.log(gamekiezer);
 	let gekozenGame = document.querySelector("input#gamechoose").value;
 	console.log(gekozenGame);
-
-	const options = {
-		method: 'GET',
-		headers: {
-			'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com',
-			'X-RapidAPI-Key': '8b3b04ef67mshb9494ce2457e3c4p130cadjsn6c5d39bb1752'
-		}
-	};
-
-	fetch('https://rawg-video-games-database.p.rapidapi.com/games', options)
-		.then(response => console.log(response))
-		.then(response => console.console.log(options))
-		.catch(err => console.error(err));
+	
 
 	if (games.includes(gekozenGame)) {
 		alert("Game wordt in database gezet");
