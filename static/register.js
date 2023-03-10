2418d8007e04bfcf35cf104a3b23cfb1daad0487
TejoBurgboy <tejovdburg@gmail.com>
2023-03-10 10:15:51 +0100

    Api toegevoegd

--- static/register.js
+++ static/register.js
@@ -1,7 +1,8 @@
 const gamekiezer = document.querySelector("#gamechoose");
 const buttonInput = document.querySelector("button");
 const games = ["Mario", "Pokemon", "Gta", "Fifa", "World of war craft", "Zelda", "Kirby", "Super Smash", "Heardstone", "League of Legends", "mario", "pokemon", "LOL", "WOWC", "minecraft", "Civ", "FM", "Football manager"];
-fetch('https://api.rawg.io/api/genres?key=cdc5e6a426f148f89089bd9f8c25dbe2')
+
+fetch('https://api.rawg.io/api/games?key=cdc5e6a426f148f89089bd9f8c25dbe2')
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
