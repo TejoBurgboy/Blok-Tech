2bdf37523584fb8b22174a80fd8b94b7b753f600
TejoBurgboy <tejovdburg@gmail.com>
2023-03-15 15:26:05 +0100

    Api toegevoegd

--- static/register.js
+++ static/register.js
@@ -2,17 +2,17 @@ const gamekiezer = document.querySelector("#gamechoose");
 const buttonInput = document.querySelector("button");
 const games = ["Mario", "Pokemon", "Gta", "Fifa", "World of war craft", "Zelda", "Kirby", "Super Smash", "Heardstone", "League of Legends", "mario", "pokemon", "LOL", "WOWC", "minecraft", "Civ", "FM", "Football manager"];
 
-fetch('https://api.rawg.io/api/games?key=cdc5e6a426f148f89089bd9f8c25dbe2')
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+
 
 
 let inputform = () => {
 	console.log(gamekiezer);
 	let gekozenGame = document.querySelector("input#gamechoose").value;
 	console.log(gekozenGame);
-	
+	fetch(`https://api.rawg.io/api/games/${gamekiezer}key=cdc5e6a426f148f89089bd9f8c25dbe2`)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
 
 	if (games.includes(gekozenGame)) {
 		alert("Game wordt in database gezet");
