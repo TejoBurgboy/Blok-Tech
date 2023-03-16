073a34685c5092fe840f5f968c2c8a19711d9d8b
TejoBurgboy <tejovdburg@gmail.com>
2023-03-16 16:34:52 +0100

    Api toegevoegd

--- static/register.js
+++ static/register.js
@@ -1,24 +1,26 @@
 const gamekiezer = document.querySelector("#gamechoose");
 const buttonInput = document.querySelector("button");
-const games = ["Mario", "Pokemon", "Gta", "Fifa", "World of war craft", "Zelda", "Kirby", "Super Smash", "Heardstone", "League of Legends", "mario", "pokemon", "LOL", "WOWC", "minecraft", "Civ", "FM", "Football manager"];
-
-
-
-
+const submitBtn = document.getElementById("submit-btn");
+const gameCheck = document.getElementById("game-check");
+submitBtn.disabled = true;
 let inputform = () => {
 	console.log(gamekiezer);
 	let gekozenGame = document.querySelector("input#gamechoose").value;
 	console.log(gekozenGame);
-	fetch(`https://api.rawg.io/api/games/${gamekiezer}key=cdc5e6a426f148f89089bd9f8c25dbe2`)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-	if (games.includes(gekozenGame)) {
-		alert("Game wordt in database gezet");
-	} else {
-		alert("Deze game bestaat niet");
-	}
+	fetch(
+		`https://api.rawg.io/api/games/${gekozenGame
+			.replace(/\s+/g, "-")
+			.toLowerCase()}?key=cdc5e6a426f148f89089bd9f8c25dbe2`
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			if (!data.detail) {
+				submitBtn.disabled = false;
+				alert("Deze game bestaat");
+			} else {
+				alert("Deze game bestaat niet");
+			}
+		})
+		.catch((err) => console.error(err), );
 };
-
-buttonInput.addEventListener("click", inputform);
+gameCheck.addEventListener("click", inputform);
